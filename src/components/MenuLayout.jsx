80154c3b1b75dcd892dcee570cecaa8d054232c6
MD. ABDULLAH AL MAMUN <58620409+abdullah-gp@users.noreply.github.com>
2023-09-11 14:25:02 +0600

    Modal Page done

--- src/components/MenuLayout.jsx
+++ src/components/MenuLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Breadcrumb from "./Breadcrumb";
 
 const productCardData = [
@@ -77,6 +77,16 @@ const productCardData = [
 ];
 
 const MenuLayout = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <>
       <div className="w-full h-8 bg-dark-primary py-8 px-6 flex justify-between items-center">
@@ -87,6 +97,7 @@ const MenuLayout = () => {
       <section className="p-6 mt-4">
         <div className="w-full h-8 bg-dark-primary p-10 pr-20 flex justify-between items-center mb-6 relative">
           <button
+            onClick={() => setShowModal(true)}
             type="button"
             className="menu_add absolute w-12 h-12 bg-brand-primary font-bold text-4xl rounded-full flex justify-center items-center -top-6 right-4"
           >
@@ -125,6 +136,121 @@ const MenuLayout = () => {
           })}
         </div>
       </section>
+
+      {/* modal section */}
+      {showModal && (
+        <div
+          className={`fixed top-0 left-0 w-full h-screen overflow-hidden bg-dark-tertiary/60 flex justify-center items-center`}
+        >
+          <div className="w-full max-w-3xl relative">
+            <button
+              type="button"
+              className="absolute -right-3 -top-8 bg-brand-primary w-6 h-6 flex justify-center items-center rounded-sm focus:outline-none group transition-all duration-300 ease  hover:rounded-full"
+              onClick={() => setShowModal(false)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 384 512"
+              >
+                <path
+                  d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+                  fill="white"
+                />
+              </svg>
+            </button>
+            <div className="bg-dark-primary p-4 ">
+              <p className="text-brand-primary">Add Menu List</p>
+            </div>
+
+            <div className="bg-dark-primary p-10 mt-4 flex items-start gap-8">
+              {/* img section */}
+              <div className="flex justify-center items-center flex-col gap-4">
+                <div className="w-24 h-24 bg-dark-tertiary rounded-full flex justify-center items-center">
+                  <img src="/images/add-item.svg" alt="" />
+                </div>
+                <p className="text-brand-primary">Add Product Image</p>
+              </div>
+              {/* input section */}
+              <div className=" space-y-4">
+                <div className="flex items-center">
+                  <label
+                    className="w-full max-w-[130px]  inline-block"
+                    htmlFor="#"
+                  >
+                    Product Name:
+                  </label>
+                  <input
+                    className="bg-transparent border ml-4 p-2 rounded-sm focus:outline-none w-72"
+                    placeholder="Item . . ."
+                    type="text"
+                  />
+                </div>
+                <div className="flex items-center">
+                  <label
+                    className="w-full max-w-[130px] inline-block"
+                    htmlFor="#"
+                  >
+                    Product Price:
+                  </label>
+                  <input
+                    className="bg-transparent border ml-4 p-2 rounded-sm focus:outline-none w-72"
+                    placeholder="$0.00"
+                    type="text"
+                  />
+                </div>
+                <div className="flex items-center">
+                  <label
+                    className="w-full max-w-[130px] inline-block"
+                    htmlFor="#"
+                  >
+                    Product Category:
+                  </label>
+                  <select
+                    className="w-full ml-4 bg-transparent border p-2 rounded-sm focus:outline-none"
+                    name=""
+                    id=""
+                  >
+                    <option className="bg-dark-tertiary" selected value="">
+                      Category. . .
+                    </option>
+                    <option className="bg-dark-tertiary" value="">
+                      Items One
+                    </option>
+                    <option className="bg-dark-tertiary" value="">
+                      Items Two
+                    </option>
+                    <option className="bg-dark-tertiary" value="">
+                      Items Three
+                    </option>
+                  </select>
+                </div>
+                <div className="flex items-center">
+                  <label
+                    className="w-full max-w-[130px] inline-block"
+                    htmlFor="#"
+                  >
+                    Product Details:
+                  </label>
+                  <textarea
+                    className="bg-transparent border ml-4 p-2 rounded-sm focus:outline-none w-72"
+                    placeholder="Description"
+                    type="textarea"
+                  />
+                </div>
+                <div className="flex justify-end">
+                  <button
+                    type="button"
+                    className="bg-brand-primary text-white rounded-full py-2 px-6 text-sm"
+                  >
+                    Menu Upload
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
